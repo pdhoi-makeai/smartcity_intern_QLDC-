922f7ba76f37a75bec19013aec496e4b7696a9cd
pdhoi-makeai <pdhoi@makeai.vn>
2026-06-20 08:24:49 +0700

    Remove test and setup scripts

--- run_doctypes_v3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-cd /home/user/Smart_City_V2/backend/v16-bench
-
-cp /home/user/Smart_City_V2/setup_doctypes_v3.py apps/quan_ly_dan_cu/quan_ly_dan_cu/setup_doctypes_v3.py
-bench --site smartcity.localhost execute quan_ly_dan_cu.setup_doctypes_v3.execute
-
-echo "Tạo mới 6 DocType thành công!"
